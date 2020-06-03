d48c8a6ceab8e4a6845b067278a704e590daf637
dardothemaster <dardo@protonmail.com>
2020-06-03 23:25:26 +0200

    C# Native Library

---
+++ c#/native library/Class1.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ArmaDotNetCore
+{
+    public class ArmaExtension
+    {
+        //This tells the compiler to create an entrypoint named 'RVExtension'. This line should be added
+        // to each method you want to export. Only public static are accepted.
+        [UnmanagedCallersOnly(EntryPoint = "RVExtension")]
+
+        //The callExtension command passes 3 parameters:
+        /*
+
+        output: Pointer to the string that will be returned to the sqf end
+        outputSize : Maximum amount of bytes allowed in the string pointed by output,make sure not to exceed this limit
+        function : Pointer to the string specified to the right of the callExtension command
+
+        */    
+        public static void RVExtension(IntPtr output, int outputSize, IntPtr function)
+        {
+            //Let's grab the string from the pointer passed from the Arma call to our extension
+            string parameter = Marshal.PtrToStringAnsi(function);
+
+            //Now we have to call the other function to reverse our string
+            char[] strToArr = reverse(parameter);
+
+            string finalString = new string(strToArr) + char.MinValue;
+
+            /*
+            Now that we have our reversed string terminated by the null character,
+            we have to convert it to a byte array in order to allow the arma extension loader (Which is c/c++)
+            to "decode" our string. We'll basically copy our string into the location pointed by the 'output' pointer.
+            */
+            byte[] byteFinalString = Encoding.ASCII.GetBytes(finalString);
+
+            //We're done, now that we have our properly encoded byte array, we have to 'assign' its value to the
+            //memory location pointed by output pointer. Aaand it's gone.
+            Marshal.Copy(byteFinalString,0,output,byteFinalString.Length);
+        }
+
+
+        [UnmanagedCallersOnly(EntryPoint = "RVExtensionVersion")]
+        public static void RVExtensionVersion(IntPtr output, int outputSize)
+        {
+            string greetingsString = "Greetings from C#! || .net core example" + Environment.NewLine + "If you see this the extension has been loaded";
+            
+            string finalString = greetingsString + char.MinValue;
+
+            byte[] byteFinalString = Encoding.ASCII.GetBytes(finalString);
+            
+            Marshal.Copy(byteFinalString,0,output,byteFinalString.Length);
+        }
+
+
+        public static char[] reverse(string parameter) {
+
+            // Convert the string to char array
+            char[] arr = parameter.ToCharArray();
+            
+            //Reverse the array
+            Array.Reverse( arr );
+
+            //Return reversed array
+            return arr;
+        }
+    }
+}
---
+++ c#/native library/README.md
@@ -0,0 +1,34 @@
+# What is this?
+A template project for writing Arma 3 extensions in .Net Core, using ILCompiler provided by CoreRT
+
+# Author
+CoreRT https://github.com/dotnet/corert
+
+## How to Setup
+1) Install DotNet core sdk and dependencies from Microsoft website ( https://dotnet.microsoft.com/download )
+
+2) Open a terminal in the project directory
+
+3) Install the native compiler
+```bash
+dotnet add package Microsoft.DotNet.ILCompiler -v 1.0.0-alpha-* 
+```
+4) Compile using
+
+  ```bash
+> dotnet publish /p:NativeLib=Shared -r win-x64|linux-x64
+```
+5) Grab your native compiled library
+```bash
+/bin/Debug/netstandard2.0/linux-x64/native/dotnet_a3.so
+/bin/Debug/netstandard2.0/win-x64/native/dotnet_a3.dll
+```
+Now the library is ready to be loaded from Arma servers / clients.
+
+Keep in mind to add the _x64 suffix, as this is a 64bit extension!
+
+## Why to use?
+Obviously this won't never run faster than native c/c++ code. The only thing you'll really get, is that you'll be able to compile an extension for both windows and linux without touching in any ways your code. Which for Server sided extensions , is probably (?) very useful. 
+
+
+> Written with [StackEdit](https://stackedit.io/).
