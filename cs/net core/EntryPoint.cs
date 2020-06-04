44120857e60264585a39a736916740432c9ea123
dardothemaster <dardo@protonmail.com>
2020-06-04 13:09:04 +0200

    Updates: triple slash

--- cs/net core/EntryPoint.cs
+++ cs/net core/EntryPoint.cs
@@ -9,15 +9,15 @@ namespace ArmaDotNetCore
         //This tells the compiler to create an entrypoint named 'RVExtension'. This line should be added
         // to each method you want to export. Only public static are accepted.
         [UnmanagedCallersOnly(EntryPoint = "RVExtension")]
-
-        //The callExtension command passes 3 parameters:
-        /*
-
-        output: Pointer to the string that will be returned to the sqf end
-        outputSize : Maximum amount of bytes allowed in the string pointed by output,make sure not to exceed this limit
-        function : Pointer to the string specified to the right of the callExtension command
-
-        */    
+        /// <summary>
+        /// This is the code that will get executed upon issuing a call to the extension from arma.
+        /// </summary>
+        /// <code>
+        /// "dotnet_a3" callExtension "ourString";
+        /// </code>
+        /// <param name="output">A pointer to the memory location of a chars array that will be read after issuing callExtension command</param>
+        /// <param name="outputSize">An integer that determines the maximum lenght of the array</param>
+        /// <param name="function">A pointer to the string passed from arma</param>
         public static void RVExtension(IntPtr output, int outputSize, IntPtr function)
         {
             //Let's grab the string from the pointer passed from the Arma call to our extension
@@ -26,7 +26,7 @@ namespace ArmaDotNetCore
             //Now we have to call the other function to reverse our string
             char[] strToArr = reverse(parameter);
 
-            string finalString = new string(strToArr) + char.MinValue;
+            string finalString = new string(strToArr) + '\0';
 
             /*
             Now that we have our reversed string terminated by the null character,
@@ -36,24 +36,34 @@ namespace ArmaDotNetCore
             byte[] byteFinalString = Encoding.ASCII.GetBytes(finalString);
 
             //We're done, now that we have our properly encoded byte array, we have to 'assign' its value to the
-            //memory location pointed by output pointer. Aaand it's gone.
+            //memory location pointed by output pointer.
             Marshal.Copy(byteFinalString,0,output,byteFinalString.Length);
         }
 
-
         [UnmanagedCallersOnly(EntryPoint = "RVExtensionVersion")]
+
+        /// <summary>
+        /// This is the code that will get executed once the extension gets loaded from arma.
+        /// The output will get printed in RPT logs
+        /// </summary>
+        /// <param name="output">A pointer to the memory location of a chars array that will be read after the load of the extension.</param>
+        /// <param name="outputSize">An integer that determines the maximum lenght of the array</param>
         public static void RVExtensionVersion(IntPtr output, int outputSize)
         {
-            string greetingsString = "|Arma .Net Core Sample|";
+            string greetingsString = "|Arma .Net Core Sample|"; //String that will get printed on RPT logs
             
-            string finalString = greetingsString + char.MinValue;
+            string finalString = greetingsString + '\0';
 
             byte[] byteFinalString = Encoding.ASCII.GetBytes(finalString);
             
             Marshal.Copy(byteFinalString,0,output,byteFinalString.Length);
         }
 
-
+        /// <summary>
+        /// This is a function to reverse a provided string and returns a chars array
+        /// </summary>
+        /// <param name="parameter">A generic string to be reversed.</param>
+        /// <returns>A chars array</returns>
         public static char[] reverse(string parameter) {
 
             // Convert the string to char array
--- cs/net core/README.md
+++ cs/net core/README.md
@@ -3,6 +3,7 @@ A template project for writing Arma 3 extensions in .Net Core, using ILCompiler 
 
 # Author
 CoreRT https://github.com/dotnet/corert
+Dardo https://github.com/DardoTheMaster
 
 ## How to Setup
 1) Install DotNet core sdk and dependencies from Microsoft website ( https://dotnet.microsoft.com/download )
@@ -20,19 +21,14 @@ dotnet add package Microsoft.DotNet.ILCompiler -v 1.0.0-alpha-*
 ```
 5) Grab your native compiled library
 ```bash
-/bin/Debug/netstandard2.0/linux-x64/native/dotnet_a3.so
-/bin/Debug/netstandard2.0/win-x64/native/dotnet_a3.dll
+/bin/Debug/netstandard2.0/linux-x64/native/dotnet_a3_x64.so
+/bin/Debug/netstandard2.0/win-x64/native/dotnet_a3_x64.dll
 ```
 Now the library is ready to be loaded from Arma servers / clients.
 
-Keep in mind to add the _x64 suffix, as this is a 64bit extension!
-
 ## Why to use?
-Obviously this won't never run faster than native c/c++ code. The only thing you'll really get, is that you'll be able to compile an extension for both windows and linux without touching in any ways your code. Which for Server sided extensions , is probably (?) very useful. 
+Obviously this won't never run faster than native c/c++ code. The only thing you'll really get, is that you'll be able to compile an extension for both windows and linux without touching in any ways your code.
 
 ## Notes
 - using '-c Release' flag while compiling will drastically reduce dll/so size
 - sometimes compiling will take a lot of time
-
-
-> Written with [StackEdit](https://stackedit.io/).
